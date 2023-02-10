26a04d35cbdcd9f028d8227f2bb5bee9c01da84f
Alejandro González Bueno <alejandro.gonzalez@civica-soft.com>
2023-02-10 19:03:47 +0000

    macro_custom_databases

--- models/staging/postgres_public/stg_postgres_public_orders.sql
+++ models/staging/postgres_public/stg_postgres_public_orders.sql
@@ -1,6 +1,9 @@
 {{
   config(
-    materialized='view'
+    materialized='incremental',
+    unique_key= 'order_id',
+    merge_update_columns = ['order_cost_eur','order_total_eur'],
+    on_schema_change = 'append_new_columns'
   )
 }}
 
@@ -12,34 +15,40 @@ with
     orders_renamed as (
 
         select
-            order_id,
-            address_id,
-            shipping_service as servicio_de_envio,
-            shipping_cost as coste_envio,
-            created_at as fecha_creacion,
-            case 
+             order_id
+            ,address_id
+            ,shipping_service 
+            ,shipping_cost
+            ,created_at
+            ,case 
                 when promo_id = '' then null
-                else {{ dbt_utils.surrogate_key('promo_id') }}
-                end as id_promo ,
-            promo_id as promo_desc,
-            estimated_delivery_at as fecha_entrega_estimada,
-            order_cost as coste_total_sin_envio_euros,
-            user_id,
-            order_total as venta_final,
-            delivered_at as fecha_entrega,
-            case 
+                else {{ dbt_utils.surrogate_key(['promo_id']) }}
+                end as id_promo
+            ,promo_id as promo_desc
+            ,estimated_delivery_at
+            ,order_cost as order_cost_eur
+            ,user_id
+            ,order_total as order_total_eur
+            ,delivered_at as delivered_at_utc
+            ,case 
                 when tracking_id = '' then null
-                else {{ dbt_utils.surrogate_key('tracking_id') }}
-                end as tracking_id,
-            {{ dbt_utils.surrogate_key('status') }} as estado_id,
-            status as estado_del_pedido
+                else {{ dbt_utils.surrogate_key(['tracking_id']) }}
+                end as tracking_id
+            ,{{ dbt_utils.surrogate_key(['status']) }} as estatus_id
+            ,status as estatus_desc
+            ,_fivetran_synced
 
         from src_orders
  
 
     )
 
-
-
 select *
 from orders_renamed
+
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  where _fivetran_synced > (select max(_fivetran_synced) from {{ this }})
+
+{% endif %}
