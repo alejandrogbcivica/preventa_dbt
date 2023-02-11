720dce5b1a422177b1d3e6b84074452f1cb1f67f
Alejandro González Bueno <alejandro.gonzalez@civica-soft.com>
2023-02-11 10:43:41 +0000

    test snapshots

---
+++ snapshots/test.sql
@@ -0,0 +1,15 @@
+{% snapshot orders_snapshot %}
+
+{{
+    config(
+      unique_key='order_id',
+      target_database= generate_database_name('silver'),
+      target_schema= generate_schema_name('snapshots'),
+      strategy='timestamp',
+      updated_at='_fivetran_synced',
+    )
+}}
+
+select * from {{ source('postgres_public', 'order_items') }}
+
+{% endsnapshot %}
