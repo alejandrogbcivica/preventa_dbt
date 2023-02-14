1c414872cef6bcbf81dc915a2f5f37a05c373a80
Alejandro González Bueno <alejandro.gonzalez@civica-soft.com>
2023-02-14 09:34:14 +0000

    test_ci_cd

--- macros/drop_target_schema_if_exists.sql
+++ macros/drop_target_schema_if_exists.sql
@@ -1,11 +1,12 @@
 {% macro drop_target_schema_if_exists(dbs_list) %}
-
 {% for db in dbs_list %}
-
 {% set target_schema = api.Relation.create(database=db, schema=target.schema) %}
-
-{{ drop_schema(target_schema) }} {{ log("Borrado schema temporal " ~ target.schema ~ , info = true) }}
-
+{{ drop_schema(target_schema) }}
+{{
+    log(
+        "Borrado schema temporal " ~ target.schema ~ " de la base de datos " ~ db,
+        info=true,
+    )
+}}
 {% endfor %}
-
 {% endmacro %}
